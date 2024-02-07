8a9f1a22611379b3ed8601cb2b7be55d375df5c6
Braun <breyes@mozilla.com>
2024-02-07 10:27:00 -0600

    feat(trino): fully functioning stack with trino from local cli working

--- src/load_data.py
+++ src/load_data.py
@@ -1,16 +1,21 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName( # type: ignore
+spark = SparkSession.builder.appName(  # type: ignore
     "Loading Test Data to Iceberg"
 ).getOrCreate()
 
+
 def load_data():
-    df = spark.read.option("inferschema", "true").csv(
-        "/home/iceberg/test_data/netflix_users_raw.csv"
+    df = (
+        spark.read.option("inferschema", "true")
+        .option("header", True)
+        .csv("/home/iceberg/test_data/netflix_users_raw.csv")
     )
     df.write.saveAsTable("netflix.users_raw", format="iceberg", mode="overwrite")
-    df = spark.read.option("inferschema", "true").csv(
-        "/home/iceberg/test_data/netflix_titles_raw.csv"
+    df = (
+        spark.read.option("inferschema", "true")
+        .option("header", True)
+        .csv("/home/iceberg/test_data/netflix_titles_raw.csv")
     )
     df.write.saveAsTable("netflix.titles_raw", format="iceberg", mode="overwrite")
 
